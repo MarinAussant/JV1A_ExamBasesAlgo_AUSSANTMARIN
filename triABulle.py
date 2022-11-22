a36132836c6d9951ab47ee25939edd5c95d4923c
Marin Aussant <118370229+MarinAussant@users.noreply.github.com>
2022-11-22 13:41:55 +0100

    Exercice 2 Check

--- triABulle.py
+++ triABulle.py
@@ -12,14 +12,21 @@ for i in range(0,nbElmtTab):
 print("-----------------------------------")
 print("Exercice 1 :")
 print(" ")
+
 print(myTable)
-print("Je choisi de permuter la valeur à l'index 3 et celle à l'index 7 :")
+
+print(" ")
+print("Je choisi de permuter la valeur à l'index 3 et celle à l'index 7.")
 valeurTemp = myTable[7]
 myTable[7] = myTable[3]
 print("Je mets la valeur à l'index 7 dans une variable temporaire 'valeurTemp' puis je mets la valeur à l'index 3 à l'index 7 :")
+
 print(myTable)
+
+print(" ")
 myTable[3] = valeurTemp
 print("Puis je mets la valeur stocké dans 'valeurTemp' à l'index 3 du tableau :")
+
 print(myTable)
 
 
@@ -28,5 +35,29 @@ print(" ")
 print(" ")
 print("-----------------------------------")
 print("Exercice 2 :")
+print(" ")
 
-#print(myTable)
+print("Tableau de base :")
+print(myTable)
+
+print(" ")
+for j in range(0,len(myTable)-1):
+
+    elmtAct = myTable[j]
+
+    if (myTable[j+1]<elmtAct):
+
+        temp = myTable[j+1]
+        myTable[j+1] = elmtAct
+        myTable[j] = temp
+
+print("Tableau rangé avec une seule itération :")
+print(myTable)
+
+
+#Exercice 2 :
+print(" ")
+print(" ")
+print("-----------------------------------")
+print("Exercice 2 :")
+print(" ")
